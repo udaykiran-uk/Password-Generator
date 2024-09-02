e0a40d4b9d2da9f647f7a6df5cfe1bac6bb859ed
mr____.uk <kanikauday00@gmail.com>
2024-09-02 16:21:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,14 +6,12 @@ const numbersEl = document.getElementById('numbers')
 const symbolsEl = document.getElementById('symbols')
 const generateEl = document.getElementById('generate')
 const clipboardEl = document.getElementById('clipboard')
-
 const randomFunc = {
     lower: getRandomLower,
     upper: getRandomUpper,
     number: getRandomNumber,
     symbol: getRandomSymbol
 }
-
 clipboardEl.addEventListener('click', () => {
     const password = resultEl.innerText;
   if (!password) {
@@ -32,7 +30,6 @@ generateEl.addEventListener('click', () => {
 
     resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length)
 })
-
 function generatePassword(lower, upper, number, symbol, length) {
     let generatedPassword = ''
     const typesCount = lower + upper + number + symbol
@@ -41,31 +38,25 @@ function generatePassword(lower, upper, number, symbol, length) {
     if(typesCount === 0) {
         return ''
     }
-
     for(let i = 0; i < length; i += typesCount) {
         typesArr.forEach(type => {
             const funcName = Object.keys(type)[0]
             generatedPassword += randomFunc[funcName]()
         })
     }
-
     const finalPassword = generatedPassword.slice(0, length)
 
     return finalPassword
 }
-
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
 }
-
 function getRandomUpper() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
 }
-
 function getRandomNumber() {
     return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
 }
-
 function getRandomSymbol() {
     const symbols = '!@#$%^&*(){}[]=<>/,.'
     return symbols[Math.floor(Math.random() * symbols.length)]
